5479847f39802375f3fc3c8528de8c261c6422de
TooMuchFish <103075501+TooMuchFish@users.noreply.github.com>
2022-05-05 16:17:04 -0700

    Update lab-report-3.m

--- lab-report-3.m
+++ lab-report-3.m
@@ -1,1 +1,10 @@
+# Name : Shaodong Shan
+# Course: CSE 15L Lab3
+Date: May 05, 2022
 
+This is my third lab report, welcome.
+![welcome](https://user-images.githubusercontent.com/103075501/162642398-9902f982-4aa5-4e33-816d-d0eba4ceace9.jpeg)
+<
+# 1. Streamlining ssh Configuration
+First step we need to Show the .ssh/config file
+we can simpily use the command "tough config" in our terminal.
